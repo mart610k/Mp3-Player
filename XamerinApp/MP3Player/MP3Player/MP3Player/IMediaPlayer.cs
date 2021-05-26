d5fa40f3dfb69a2d56e454c6eda7510d7bbf9d73
darkmtbg2 <darkmtbg2@gmail.com>
2021-05-26 09:25:52 +0200

    Comments and moved some files around

--- XamerinApp/MP3Player/MP3Player/MP3Player/IMediaPlayer.cs
+++ XamerinApp/MP3Player/MP3Player/MP3Player/IMediaPlayer.cs
@@ -6,14 +6,31 @@ namespace MP3Player
 {
     public interface IMediaPlayer
     {
+        /// <summary>
+        /// File location of the current playing track
+        /// </summary>
         string FileLocation { get; }
 
+        /// <summary>
+        /// select track by filename
+        /// </summary>
+        /// <param name="fileName">Filename to select</param>
         void SelectTrack(string fileName);
 
+        /// <summary>
+        /// PLay and pause the currently playing track
+        /// </summary>
         void PlayPauseAudioFile();
 
+        /// <summary>
+        /// Select a playlist to play
+        /// </summary>
+        /// <param name="playList">Playlist object to handle tracks</param>
         void SelectPlayList(IPlayList playList);
 
+        /// <summary>
+        /// Skips the currently playing track and proceeds to the next track
+        /// </summary>
         void SkipTrack();
     }
 }
--- XamerinApp/MP3Player/MP3Player/MP3Player/IPlayList.cs
+++ XamerinApp/MP3Player/MP3Player/MP3Player/IPlayList.cs
@@ -6,12 +6,29 @@ namespace MP3Player
 {
     public interface IPlayList
     {
+        /// <summary>
+        /// Adds a track into the playlist
+        /// </summary>
+        /// <param name="track">FileName of the track</param>
         void AddTrack(string track);
 
+        /// <summary>
+        /// Removes a track from the playlist
+        /// </summary>
+        /// <param name="track">The track name to remove</param>
         void RemoveTrack(string track);
 
+        /// <summary>
+        /// Adds multiple tracks to the playlist
+        /// </summary>
+        /// <param name="tracks"></param>
         void AddTracks(string[] tracks);
 
+
+        /// <summary>
+        /// Selects the next track for playback
+        /// </summary>
+        /// <returns>The next track name to play</returns>
         string NextTrack();
 
         
