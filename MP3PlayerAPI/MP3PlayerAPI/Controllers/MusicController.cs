911fd01ff9ec4e9bf8acabba23d44d2b0667484d
darkmtbg2 <martin-baage@hotmail.com>
2021-05-31 07:52:42 +0200

    API Beginnings

---
+++ MP3PlayerAPI/MP3PlayerAPI/Controllers/MusicController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+
+namespace MP3PlayerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> DownloadFile()
+        {
+            
+
+            var stream = System.IO.File.OpenRead("Filepath");
+            FileInfo fileInfo = new FileInfo("Filepath");
+            
+            return File(stream, "application/octet-stream", fileInfo.Name);
+
+        }
+
+    }
+}
